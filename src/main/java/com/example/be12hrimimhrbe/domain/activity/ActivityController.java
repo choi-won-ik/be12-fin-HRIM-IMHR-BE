b111f77ffe07d938e9a958e4688c003348c3b46c
Inuk Choi <iu.choi3197@gmail.com>
2025-04-04 05:17:05 +0900

    IB-4 [Feat] 봉사기부등 활동관리 api 추가

--- src/main/java/com/example/be12hrimimhrbe/domain/activity/ActivityController.java
+++ src/main/java/com/example/be12hrimimhrbe/domain/activity/ActivityController.java
@@ -1,4 +1,15 @@
 package com.example.be12hrimimhrbe.domain.activity;
 
+import com.example.be12hrimimhrbe.domain.activity.model.ActivityDto;
+import com.example.be12hrimimhrbe.global.response.BaseResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/activity")
 public class ActivityController {
+    @GetMapping("/myactivity")
+    public ResponseEntity<BaseResponse<ActivityDto.ActivityListResponse>> getMyActivity(@RequestBody ActivityDto.ActivityListRequest dto) {
+        return ResponseEntity.ok().body(new BaseResponse<>(null, null));
+    }
 }
