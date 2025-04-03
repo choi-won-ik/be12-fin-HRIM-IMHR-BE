faf199af3b9ddd9d041cac60c5e0c3c950f9ce87
InukChoi <iu.choi3197@gmail.com>
2025-04-03 12:40:24 +0900

    [Feat] 마이페이지 api 추가

--- src/main/java/com/example/be12hrimimhrbe/domain/member/model/MemberDto.java
+++ src/main/java/com/example/be12hrimimhrbe/domain/member/model/MemberDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class MemberDto {
     @Getter @Builder @AllArgsConstructor @NoArgsConstructor
     public static class FindIdRequest {
@@ -22,5 +26,30 @@ public class MemberDto {
     public static class ResetPasswordRequest {
         public String oldPassword;
         public String newPassword;
+    }
+
+    @Getter @Builder @AllArgsConstructor @NoArgsConstructor
+    public static class InfoResponse {
+        public Long idx;
+        public String name;
+        public String email;
+        public String company;
+        public String department;
+        public String role;
+    }
+
+    @Getter @Builder @AllArgsConstructor @NoArgsConstructor
+    public static class ActivityResponse {
+        public Long member_idx;
+        public List<ActivityItem> activities;
+    }
+
+    @Getter @Builder @AllArgsConstructor @NoArgsConstructor
+    public static class ActivityItem {
+        public Long activityIdx;
+        public Long campaignIdx;
+        public String type;
+        public String content;
+        public LocalDate date;
     }
 }
