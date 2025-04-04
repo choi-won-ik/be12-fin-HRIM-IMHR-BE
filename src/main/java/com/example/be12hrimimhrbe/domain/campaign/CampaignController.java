c217853acdeef2e8395618b4ee0e8f76f1bbb027
choi-won-ik <whwwhs7837@gmail.com>
2025-04-04 15:03:58 +0900

    be-20-Feat-campaignDetail

--- src/main/java/com/example/be12hrimimhrbe/domain/campaign/CampaignController.java
+++ src/main/java/com/example/be12hrimimhrbe/domain/campaign/CampaignController.java
@@ -3,6 +3,8 @@ package com.example.be12hrimimhrbe.domain.campaign;
 import com.example.be12hrimimhrbe.domain.campaign.model.CampaignDto;
 import com.example.be12hrimimhrbe.domain.member.model.Member;
 import com.example.be12hrimimhrbe.global.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/campaign")
+@Tag(name = "캠페인 관리 기능")
 public class CampaignController {
     @GetMapping("/mycampaign")
     public ResponseEntity<BaseResponse<CampaignDto.CampaignListResponse>> getCampaigns(@AuthenticationPrincipal Member member) {
@@ -21,6 +24,12 @@ public class CampaignController {
     @GetMapping("/detail/{idx}")
     public ResponseEntity<BaseResponse<CampaignDto.CampaignItemResponse>> getCampaignItem(@PathVariable Integer idx,
                                                                                           @AuthenticationPrincipal Member member) {
+        return ResponseEntity.ok().body(new BaseResponse<>(null, null));
+    }
+
+    @GetMapping("/campaign/{idx}")
+    @Operation(summary = "캠페인 상세", description = "캠페인 상세 페이지 입니다.")
+    public ResponseEntity<BaseResponse<CampaignDto.CampaignDetailResp>> campaignDetail(@PathVariable Long idx) {
         return ResponseEntity.ok().body(new BaseResponse<>(null, null));
     }
 }
