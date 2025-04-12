b6eab284af53d016328feaa33a7d8427f8401037
kks <skwka12346@gmail.com>
2025-04-12 15:36:36 +0900

    [Fix] 제품 등록 기능 수정

--- src/main/java/com/example/be12hrimimhrbe/domain/product/ProductRepository.java
+++ src/main/java/com/example/be12hrimimhrbe/domain/product/ProductRepository.java
@@ -1,21 +1,9 @@
 package com.example.be12hrimimhrbe.domain.product;
 
 import com.example.be12hrimimhrbe.domain.product.model.Product;
-import com.example.be12hrimimhrbe.domain.activity.model.Activity;
-import com.example.be12hrimimhrbe.domain.product.model.ProductDto;
-import com.example.be12hrimimhrbe.domain.member.model.Member;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-public interface ProductRepository extends JpaRepository<Activity, Long> {
-
-    List<Activity> findByMember(Member member);
-
-    @EntityGraph(attributePaths = {"activity","activity.member"})
-    @Query("SELECT a FROM Activity a " +
-            "LEFT JOIN a.member m ")
-    List<Activity> findAllAndMember();
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
