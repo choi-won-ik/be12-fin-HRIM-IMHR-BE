062c98b7748a268e01eb531021d860e4f7994f88
LimChangeun <cksrms1030@naver.com>
2025-04-14 15:18:30 +0900

    IB-43 시큐리티 컴포넌트 수정

--- src/main/java/com/example/be12hrimimhrbe/global/config/SecurityConfig.java
+++ src/main/java/com/example/be12hrimimhrbe/global/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/member/login", "/member/signup/*","/member/logout","/activity/detail/*","/img/**",
                                 "/member/find-id", "/member/find-pw", "/member/reset-pw",
-                                "/error", "/swagger-ui/**", "/v3/api-docs/**", "/event/**",
+                                "/error", "/swagger-ui/**", "/v3/api-docs/**",
                                 "/swagger-resources/**").permitAll()
                         .anyRequest().authenticated()
         );
