cf0ba42e6d51343126b2ffd8ffc4a04c5b79e780
LimChangeun <cksrms1030@naver.com>
2025-04-15 15:37:12 +0900

    IB-65 이벤트 수정

--- src/main/java/com/example/be12hrimimhrbe/global/config/SecurityConfig.java
+++ src/main/java/com/example/be12hrimimhrbe/global/config/SecurityConfig.java
@@ -76,7 +76,6 @@ public class SecurityConfig {
                                 "/member/login", "/member/signup/*","/member/logout","/activity/detail/*","/img/**",
                                 "/member/find-id", "/member/find-pw", "/member/reset-pw",
                                 "/error", "/swagger-ui/**", "/v3/api-docs/**",
-//                                "/event/**",
                                 "/swagger-resources/**").permitAll()
                         .anyRequest().authenticated()
         );
