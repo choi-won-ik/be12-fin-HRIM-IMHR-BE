5a9449b8745e120cc600a19a458598e2079e1b6a
choi-won-ik <whwwhs7837@gmail.com>
2025-04-16 11:30:23 +0900

    [Fix] 알림 무한 스크롤 추가 IF-55

--- src/main/java/com/example/be12hrimimhrbe/domain/activity/ActivityRepository.java
+++ src/main/java/com/example/be12hrimimhrbe/domain/activity/ActivityRepository.java
@@ -12,17 +12,21 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
-
+    @EntityGraph(attributePaths = {"member"})
+    @Query("SELECT a FROM Activity a " +
+            "LEFT JOIN a.member m ")
     Page<Activity> findByMember(Member member, Pageable pageable);
+
     List<Activity> findAllByMember(Member member);
 
-    @EntityGraph(attributePaths = {"member"})
-    @Query("SELECT a FROM Activity a " +
-            "LEFT JOIN a.member m ")
-    List<Activity> findAllAndMember();
+//    @EntityGraph(attributePaths = {"member"})
+//    @Query("SELECT a FROM Activity a " +
+//            "LEFT JOIN a.member m ")
+//    List<Activity> findAllAndMember();
 
     @EntityGraph(attributePaths = {"member"})
     @Query("SELECT a FROM Activity a " +
-            "LEFT JOIN a.member m ")
+            "LEFT JOIN a.member m " +
+            "where a.idx= :idx")
     Activity findByIdAndMember(Long idx);
 }
