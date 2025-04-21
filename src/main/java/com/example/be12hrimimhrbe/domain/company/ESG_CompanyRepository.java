66d902ce2744bf13cdb2f9eb8aa9179f245fadfa
LimChangeun <cksrms1030@naver.com>
2025-04-21 11:08:04 +0900

    IB-96 ESG_Company Service, Repository 생성

---
+++ src/main/java/com/example/be12hrimimhrbe/domain/company/ESG_CompanyRepository.java
@@ -0,0 +1,7 @@
+package com.example.be12hrimimhrbe.domain.company;
+
+import com.example.be12hrimimhrbe.domain.company.model.ESG_Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ESG_CompanyRepository extends JpaRepository<ESG_Company, Long> {
+}
