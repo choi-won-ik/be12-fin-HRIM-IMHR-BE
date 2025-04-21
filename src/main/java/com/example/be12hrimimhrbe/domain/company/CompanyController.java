346ae611d24366fa754f816a8e846bf5dff4a2d6
LimChangeun <cksrms1030@naver.com>
2025-04-21 11:03:23 +0900

    IB-96 ESG_Company Dto 생성 및 작성

--- src/main/java/com/example/be12hrimimhrbe/domain/company/CompanyController.java
+++ src/main/java/com/example/be12hrimimhrbe/domain/company/CompanyController.java
@@ -1,4 +1,13 @@
 package com.example.be12hrimimhrbe.domain.company;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/company")
+@Tag(name = "회사 관리 기능")
 public class CompanyController {
 }
