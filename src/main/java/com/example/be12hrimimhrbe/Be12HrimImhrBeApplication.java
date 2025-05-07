ff3c8c1bad8e9d7fe0a8ff268730b051aa8f49e4
ICG <cksrms1030@naver.com>
2025-05-07 17:52:14 +0900

    IB-140 기본적인 컴포넌트 생성

--- src/main/java/com/example/be12hrimimhrbe/Be12HrimImhrBeApplication.java
+++ src/main/java/com/example/be12hrimimhrbe/Be12HrimImhrBeApplication.java
@@ -2,8 +2,10 @@ package com.example.be12hrimimhrbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableMongoRepositories
 @SpringBootApplication
 @EnableScheduling // 스케줄링 어노테이션 활성화 설정
 public class Be12HrimImhrBeApplication {
