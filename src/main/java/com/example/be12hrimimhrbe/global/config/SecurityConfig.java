8455b719bf037e157fa1cf4b6dd4335a965d3987
choi-won-ik <whwwhs7837@gmail.com>
2025-05-09 11:29:01 +0900

    [Fix] 프로메테우스 security 접근

--- src/main/java/com/example/be12hrimimhrbe/global/config/SecurityConfig.java
+++ src/main/java/com/example/be12hrimimhrbe/global/config/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
                                 "/ws/**", "/app/**", "/topic/**",
                                 "/member/login", "/member/signup/*","/member/logout","/activity/detail/*","/img/**",
                                 "/member/find-id", "/member/find-pw", "/member/reset-pw", "/actuator/health",
-                                "/error", "/swagger-ui/**", "/v3/api-docs/**","/metrics",
+                                "/error", "/swagger-ui/**", "/v3/api-docs/**","/actuator/prometheus",
                                 "/swagger-resources/**").permitAll()
                         .anyRequest().authenticated()
         );
