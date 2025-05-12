8bf6d95839c9a683477314ec08b7bdfd229fb98c
choi-won-ik <whwwhs7837@gmail.com>
2025-05-12 09:47:45 +0900

    [Fix] batch test

--- src/main/java/com/example/be12hrimimhrbe/global/config/ScheduleConfig.java
+++ src/main/java/com/example/be12hrimimhrbe/global/config/ScheduleConfig.java
@@ -17,7 +17,7 @@ public class ScheduleConfig {
 //    // 매 시간 정각
 //    @Scheduled(cron = "0 32 * * * *", zone = "Asia/Seoul")
     // 매월 첫 쨰주 월용일 00:00:10
-    @Scheduled(cron = "0 2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58 * * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 5,10,15,20,25,30,35,40,45,50,55 * * * *", zone = "Asia/Seoul")
     public void runFirstJob() throws Exception {
 
         log.info("first schedule start");
